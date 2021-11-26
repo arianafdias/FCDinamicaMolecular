c7252e145648b7a3f803e679dc6cc0505e617c9c
arianafdias <60117548+arianafdias@users.noreply.github.com>
2021-11-26 22:17:09 +0000

    Add files via upload

--- dinamicV2.cpp
+++ dinamicV2.cpp
@@ -2,13 +2,15 @@
 #include <fstream>
 #include <vector>
 #include <math.h>
-#include<numeric>
+#include <numeric>
 #include <iomanip> 
+#include <string> 
+#include <stdlib.h> 
 using namespace std;
 
 //global vars
 double G = 4.49235*pow(10,-15); // pc Msolar^-1 (pc/ano)^2
-double deltaT = 2*pow(10,4); // "erro numerico"
+double deltaT = 2*pow(10,4); // "erro numerico"   // 4.1 -> 2*pow(10,4)
 double pcConv = 1.02269032*pow(10, -6); //conversão em parsec
 struct vetor {double x; double y; double z;};
 struct estrela {vetor x0; vetor x1; vetor x2; vetor v; double m;};
@@ -84,7 +86,7 @@ vetor calculo_aceleracao(int npart, vector<estrela> sistema, int t, int particul
 	
 	for (int i = 0; i < npart; i++){
 		if (i == particulaEmQuestao) continue;
-		//cout << i << particulaEmQuestao  << "\t" << t << endl;
+		
 		if (t == 1) {
 			F.x += (G * (sistema[particulaEmQuestao].m * sistema[i].m)* (sistema[particulaEmQuestao].x0.x - sistema[i].x0.x)) / pow(distancia(sistema[i].x0, sistema[particulaEmQuestao].x0), 3.0);
 			F.y += (G * (sistema[particulaEmQuestao].m * sistema[i].m)* (sistema[particulaEmQuestao].x0.y - sistema[i].x0.y)) / pow(distancia(sistema[i].x0, sistema[particulaEmQuestao].x0), 3.0);
@@ -94,11 +96,17 @@ vetor calculo_aceleracao(int npart, vector<estrela> sistema, int t, int particul
 			F.x += (G * (sistema[particulaEmQuestao].m * sistema[i].m)* (sistema[particulaEmQuestao].x1.x - sistema[i].x1.x)) / pow(distancia(sistema[i].x1, sistema[particulaEmQuestao].x1), 3.0);
 			F.y += (G * (sistema[particulaEmQuestao].m * sistema[i].m)* (sistema[particulaEmQuestao].x1.y - sistema[i].x1.y)) / pow(distancia(sistema[i].x1, sistema[particulaEmQuestao].x1), 3.0);
 			F.z += (G * (sistema[particulaEmQuestao].m * sistema[i].m)* (sistema[particulaEmQuestao].x1.z - sistema[i].x1.z)) / pow(distancia(sistema[i].x1, sistema[particulaEmQuestao].x1), 3.0);
-		} 
+		}
+		
+		
+		
+		
 	}
 	a.x = -F.x / sistema[particulaEmQuestao].m;
 	a.y = -F.y / sistema[particulaEmQuestao].m;
 	a.z = -F.z / sistema[particulaEmQuestao].m;	
+	
+	
 	
 	return a;
 }
@@ -140,6 +148,7 @@ vetor velocity(vector<estrela> sistema, int particulaEmQuestao){
 }
 
 
+// ---------------- EXERCICIO 4.1 ---------------- //
 
 int ex41(){
 
@@ -239,7 +248,7 @@ int ex41(){
 	EnergiaP.push_back(potencial(npart, sistema, currentT));
 	EnergiaC.push_back(cinetica(npart, sistema));
 	}
-	cout << EnergiaP.size() << "\t" << EnergiaC.size() << endl;
+	
 	movfile.close();
 	ofstream file("Energia.txt");
 	for(int i = 0; i < EnergiaC.size(); i++){
@@ -251,8 +260,251 @@ int ex41(){
 	return 0;
 }
 
+// ---------------- EXERCICIO 4.2 ---------------- //
+vetor centroMassaAglomerado (vector<estrela> sistema, int t){
+
+	vetor CM;
+	
+	double mTotal = 0;
+	for(int i = 0; i < sistema.size(); i++){
+		mTotal += sistema[i].m;
+		}
+		
+	double sumx = 0;
+	double sumy = 0;
+	double sumz = 0;
+	for(int i = 0; i < sistema.size(); i++){
+		if (t == 0){
+			sumx += sistema[i].m*sistema[i].x0.x;
+			sumy += sistema[i].m*sistema[i].x0.y;
+			sumz += sistema[i].m*sistema[i].x0.z;
+		}else if(t == 1){
+			sumx += sistema[i].m*sistema[i].x1.x;
+			sumy += sistema[i].m*sistema[i].x1.y;
+			sumz += sistema[i].m*sistema[i].x1.z;
+		}else{
+			sumx += sistema[i].m*sistema[i].x2.x;
+			sumy += sistema[i].m*sistema[i].x2.y;
+			sumz += sistema[i].m*sistema[i].x2.z;
+		}
+	}
+	
+	CM.x = sumx/mTotal;
+	CM.y = sumy/mTotal;
+	CM.z = sumz/mTotal;
+			
+	return CM;
+
+}
+
+int nrEstrelasDMenorRt (vector<estrela> sistema, int t){
+	
+	int nrEstrelas = 0;
+	double d;
+	double rT = 5; //pc
+	
+	vetor rCM = centroMassaAglomerado(sistema, t);
+	for(int i = 0; i < sistema.size(); i++){
+		if(t == 0) d = distancia(rCM,sistema[i].x0);
+		else if(t == 1) d = distancia(rCM,sistema[i].x1);
+		else d = distancia(rCM,sistema[i].x1);
+		
+		if (d < rT) nrEstrelas++;
+	}
+
+	return nrEstrelas;
+}
+
+
+int ex42(){
+
+	// ---- TIRAR INFO DO FILE ---- //
+	ifstream myfile;
+	myfile.open("500.dat");
+	int npart = 500;
+	estrela newStar;
+	vector<estrela> sistema(npart, newStar);
+	int idx = 0;
+	while (!myfile.eof()){
+		myfile >> sistema[idx].x0.x;
+		myfile >> sistema[idx].x0.y;
+		myfile >> sistema[idx].x0.z;
+		myfile >> sistema[idx].v.x;
+		myfile >> sistema[idx].v.y;
+		myfile >> sistema[idx].v.z;
+		myfile >> sistema[idx].m;
+		idx++;
+	}
+	
+	
+	
+	// ------- SIMULAÇÃO ------- //
+	int tMAX = 10*pow(10,6)/deltaT;
+	vector<int> nrTotalEstrelasMenorRt;
+	
+	
+	// SISTEMA SITUA-SE EM T = 0
+	int currentT = 0;
+	// AS POSIÇÕES INICIAIS SÃO RETIRADAS DA LEITURA DO FICHEIRO
+	
+	nrTotalEstrelasMenorRt.push_back(nrEstrelasDMenorRt(sistema, currentT));
+	currentT++;
+	
+	
+	// SISTEMA SITUA-SE EM T = 1
+	// CALCULA-SE AS POSIÇOES EM T = 1
+	for(int n = 0; n < npart; n++){
+		sistema[n].x1 = calculo_x1(npart, sistema, currentT, n);
+	}
+	
+	// CALCULA-SE O CENTRO DE MASSA DO SISTEMA EM T = 1
+	// VERIFICA-SE AS DISTANCIAS DAS ESTRELAS AO CM DO AGLOMERADO
+	nrTotalEstrelasMenorRt.push_back(nrEstrelasDMenorRt(sistema, currentT));
+	currentT++;
+	
+	
+	// SISTEMA SITUA-SE EM T = 2
+	// CALCULA-SE AS POSIÇOES EM T = 2
+	for(int n = 0; n < npart; n++){
+		sistema[n].x2 = calculo_xn(npart, sistema, currentT, n);
+	}
+	
+	// CALCULA-SE O CENTRO DE MASSA DO SISTEMA EM T = 2
+	// VERIFICA-SE AS DISTANCIAS DAS ESTRELAS AO CM DO AGLOMERADO
+	nrTotalEstrelasMenorRt.push_back(nrEstrelasDMenorRt(sistema, currentT));
+	currentT++;
+	
+	
+	
+	// SISTEMA SITUA-SE EM T > 2
+	for(int t = currentT; t < tMAX; t ++){
+		for( int n = 0; n < npart; n++){
+			sistema[n].x0 = sistema[n].x1;
+			sistema[n].x1 = sistema[n].x2;
+		}	
+		
+		for(int n = 0; n < npart; n++){
+			sistema[n].x2 = calculo_xn(npart, sistema, t, n);
+			/*
+			ofstream myFile("EstrelaNr" + to_string(n) + ".txt", fstream::app);
+			myFile << sistema[n].x2.x << "\t" << sistema[n].x2.y << "\t" << sistema[n].x2.z << endl;
+			myFile.close();
+			*/
+			
+			
+		}
+		
+		nrTotalEstrelasMenorRt.push_back(nrEstrelasDMenorRt(sistema, t));
+	 }
+	
+	ofstream file("Distruibuicoes"+to_string(npart)+".txt");
+	for(int i = 0; i < nrTotalEstrelasMenorRt.size(); i++){
+		file << (double) nrTotalEstrelasMenorRt[i] / (double) npart << endl;
+	}
+	
+	
+	
+	file.close();
+
+
+
+	return 0;
+}
+
+
+// ---------------- EXERCICIO 4.3 ---------------- //
+
+double gerarMassa() {
+	
+	double m;
+	double c = 0.205827;
+	double random = drand48();
+	double alpha1 = 0.3;
+	double alpha2 = 1.3;
+	double alpha3 = 2.3;
+	
+	if (0 < random && random <= 0.0384783) {
+		m = pow((((1-alpha1)/c)*random + pow(0.01, (1-alpha1))),(1/(1-alpha1)));
+	} else if (0.0384783 < random && random <= 0.610547) {
+		m = pow(((1-alpha2)*( random/c - pow(0.08,(1-alpha1))/(1-alpha1) + pow(0.01,(1-alpha1))/(1-alpha1) + pow(0.08,(1-alpha2))/(1-alpha2))),(1/(1-alpha2)));
+	} else if (0.610547 < random && random <= 1){
+		m = pow(    (1-alpha3)*( random/ c -pow(0.08,(1-alpha1))/(1-alpha1) +pow(0.01,(1-alpha1))/(1-alpha1) -pow(0.5,(1-alpha2))/(1-alpha2) + pow(0.08,(1-alpha2))/(1-alpha2) + pow(0.5,(1-alpha3))/(1-alpha3)    ), (1 / (1-alpha3))) ;
+	}
+	
+	return m;
+}
+
+vector<estrela> gerarPosicoesIniciais(vector<estrela> sistema){
+	
+	vector<double> amostrasAceitaveisParaR;
+	
+	while (amostrasAceitaveisParaR.size() < npart){
+		double randomX = drand48();	
+		double randomY = drand48();
+		double OrdenadaYdeRandomX = PI(randomX);
+		
+		if (randomY <= OrdenadaYdeRandomX) amostrasAceitaveisParaR.push_back(randomX);
+		
+	}
+	
+	vector<double> thetas;
+	vector<double> phis;
+	for (int i = 0; i < npart; i++){
+		double theta = 2* M_PI * amostrasAceitaveisParaR;
+		thetas.push_back(theta);
+		double phi = acos(2 * amostrasAceitaveisParaR - 1);
+		phis.push_back(phi);
+		
+	}	
+	
+	for(int i = 0; i < npart; i++){
+		sistema[i].x0.x = amostrasAceitaveisParaR[i] * cos(thetas[i]) * sin(phis[i]);
+		sistema[i].x0.y = amostrasAceitaveisParaR[i] * sin(thetas[i]) * sin(phis[i]);
+		sistema[i].x0.z = amostrasAceitaveisParaR[i] * cos(phis[i]);
+			
+	return sistema;
+}
+
+
+
+double PI(double r){
+	
+	double PIr = (asinh(r) - r / sqrt(26))/ ((sqrt(26) * asinh(5) - 5 )/sqrt(26)) ;
+	return PIr;
+	}
+
+int ex43(){ 
+
+	int npart = 100;
+	estrela newStar;
+	vector<estrela> sistema(npart, newStar);
+	
+	for(int i = 0; i < npart; i++){
+		
+		sistema[i].m = gerarMassa();
+		//cout << sistema[i].m  << endl;
+	
+	}
+	
+	sistema = gerarPosicoesIniciais(sistema);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
 
 int main(){
-	ex41();
+	//ex41();
+	//ex42();
+	ex43();
 	return 0;
 }
